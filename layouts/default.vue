d23bc1785b07c6a7d4c8b5180514f6a253295d30
rca <nem@nem.x0.to>
2024-10-11 14:01:33 +0900

    improve

--- layouts/default.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="min-h-screen bg-gray-100">
-    <UContainer>
-      <UHeader>
-        <template #left>
-          <h1 class="text-2xl font-bold">Flowmodoro Timer</h1>
-        </template>
-        <template #right>
-          <UButton
