5316b270dfc34af2882e86958ce85ae16a01ea08
rca <nem@nem.x0.to>
2024-10-11 22:41:27 +0900

    remove unused

--- src/pages/TaskManagerPage.tsx
+++ src/pages/TaskManagerPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import TaskManager from '../components/TaskManager';
 import useApi from '../hooks/useApi';
 
