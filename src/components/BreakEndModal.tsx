26dbfa5f5c3e89a1d9618d5dea1438532a4e9bbb
rca <nem@nem.x0.to>
2024-10-11 13:55:11 +0900

    fix!

--- src/components/BreakEndModal.tsx
+++ src/components/BreakEndModal.tsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button } from "@nextui-org/react";
 import { useBreakTimer } from '../contexts/BreakTimerContext';
 import { useSessionHistory } from '../contexts/SessionHistoryContext';
+import { useStopwatch } from 'react-timer-hook';
 
 const BreakEndModal: React.FC = () => {
   const { showBreakEndModal, setShowBreakEndModal, stopBreak, currentBreakDuration } = useBreakTimer();
   const { addSessionRecord } = useSessionHistory();
 
   const handleEndBreak = () => {
-    addSessionRecord('break', currentBreakDuration, 1);
+    addSessionRecord('break', currentBreakDuration, seconds);
+    reset(undefined, false);
     stopBreak();
     setShowBreakEndModal(false);
   };
+
+  const {
+    seconds,
+    reset,
+    start
+  } = useStopwatch({ autoStart: false });
+
+  const formatTime = (seconds: number) => {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+
+    return `${hours.toString().padStart(2, '0')}:${minutes
+      .toString()
+      .padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
+  };
+
+  // showBreakEndModal を監視して, trueになったときストップウォッチをスタート
+  useEffect(() => {
+    if (showBreakEndModal) {
+      start();
+    }
+  }, [showBreakEndModal]);
 
   return (
     <Modal isOpen={showBreakEndModal} onClose={() => setShowBreakEndModal(false)}>
@@ -19,7 +44,7 @@ const BreakEndModal: React.FC = () => {
         <ModalHeader>Break Over</ModalHeader>
         <ModalBody>
           <p className="text-center text-red-600 text-3xl font-bold">
-            Over Time
+            {formatTime(seconds)}
           </p>
           <p>The break time has ended. Please wrap up.</p>
         </ModalBody>
